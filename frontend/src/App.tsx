41f314e795646655c8e146a76768f96f4a11ffe0
Cristian Pereyra <cp.crist.pereyra@gmail.com>
2024-11-09 16:27:57 -0400

    Add frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from './components/ui/button';
+
+function App() {
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>Hi</h1>
+      <Button>Hi again</Button>
+    </>
+  );
+}
+
+export default App;
