ccf2517e0c07723610bcae66d67e6a87c6e96faa
Cristian Pereyra <cp.crist.pereyra@gmail.com>
2024-11-09 16:44:13 -0400

    Add auth setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,24 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from '@clerk/clerk-react';
 import { Button } from './components/ui/button';
 
 function App() {
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>Hi</h1>
-      <Button>Hi again</Button>
+      <header>
+        <SignedOut>
+          <SignInButton>
+            <Button>Sign In</Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
     </>
   );
 }
