209cc9769e2124a31c72328c49f3e8135379ce73
Cristian Pereyra <cp.crist.pereyra@gmail.com>
2024-11-10 22:05:52 -0400

    Add admin routes and controllers

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { clerkMiddleware } from '@clerk/express';
+import fileUpload from 'express-fileupload';
+import path from 'path';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import adminRoutes from './routes/admin.route.js';
@@ -8,15 +10,24 @@ import songRoutes from './routes/song.route.js';
 import albumRoutes from './routes/album.route.js';
 import statRoutes from './routes/stat.route.js';
 import { connectDB } from './lib/db.js';
+import { error } from 'console';
 
 dotenv.config();
 
+const __dirname = path.resolve();
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
-
 app.use(clerkMiddleware());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, 'tmp'),
+    createParentPath: true,
+    limits: { fileSize: 10 * 1024 * 1024 }, // 10MB
+  })
+);
 
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
@@ -25,6 +36,18 @@ app.use('/api/songs', songRoutes);
 app.use('/api/albums', albumRoutes);
 app.use('/api/albums', albumRoutes);
 app.use('/api/stats', statRoutes);
+
+app.use((err, req, res, next) => {
+  console.error(error);
+  res
+    .status(500)
+    .json({
+      message:
+        process.env.NODE_ENV === 'production'
+          ? 'Internal server error'
+          : err.message,
+    });
+});
 
 app.listen(PORT, () => {
   console.log('Server is running on port ' + PORT);
